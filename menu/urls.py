d27b8ddbc84ab1cba7449c7daf1e082e9ecd9aee
pradhumnts <pradhumnts@gmail.com>
2021-09-15 23:52:48 +0530

    added homepage

--- menu/urls.py
+++ menu/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # ex: /menu/
     path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
     path('additem', views.add_item, name='additem'),
 ]
--- menu/views.py
+++ menu/views.py
@@ -4,6 +4,9 @@ from .models import Category, MenuItem
 
 # Create your views here.
 def index(request):
+    return render(request, 'menu/index.html')
+    
+def menu(request):
     menu_items =  MenuItem.objects.filter(active=True)
     category = Category.objects.filter(active=True)
     context = {
